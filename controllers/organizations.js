3127671dfc4346ca2de23a2b74f3f26a1814e282
JuanCarlosMtz <A01612967@itesm.mx>
2022-11-13 03:00:02 -0600

    small changes

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -12,7 +12,7 @@ module.exports = {
 
     verifyOrg : async (req, res, next) => {
         try {
-            const org = await OrgsService.getOrg(req.body);
+            const org = await OrgsService.verifytOrg(req.body);
             res.status(200).json(org)
         } catch (err) {
             res.status(500).json({"message": `Error while getting organization. Err: ${err}`});
