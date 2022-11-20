ab67aecf59ca2f46fc1a866e54f32c0194fbf0ec
JuanCarlosMtz <A01612967@itesm.mx>
2022-11-19 23:00:00 -0600

    Update employees.js

--- controllers/employees.js
+++ controllers/employees.js
@@ -117,9 +117,25 @@ module.exports = {
     },
 
     updatePassword : async (req, res, next) => {
+        const password = await EmployeesService.getPassword(req.body.username);
         try {
-            const data = await EmployeesService.updatePassword(req.body);
-            res.status(200).json(data)
+            if (await bcrypt.compare(req.body.oldPassword, password[0].password)) {
+                const hashedNewPassword = await bcrypt.hash(req.body.newPassword, 10);
+                    const employeeData = {
+                        id: req.body.id,
+                        password: hashedNewPassword
+                    };
+                const data = await EmployeesService.updatePassword(employeeData);
+                res.status(200).json(data)
+            } else {
+                const response = [
+                    {
+                        id: -1,
+                        message : "Not allowed"
+                    }
+                ];
+                res.json(response);
+            } 
         } catch (err) {
             res.status(500).json({"message": `Error while getting data. Err: ${err}`});
         }
